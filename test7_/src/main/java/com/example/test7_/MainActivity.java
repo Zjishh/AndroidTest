cae2a3759eed7a5d50433f63d2f1a4399ce7ed9c
38580 <385802188@qq.com>
2023-04-26 08:10:53 +0800

    4.20

---
+++ test7_/src/main/java/com/example/test7_/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.test7_;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        View bt = findViewById(R.id.bt);
+
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(MainActivity.this, ListView1.class);
+                intent.putExtras(intent);
+                startActivity(intent);
+            }
+        });
+        View bt2 = findViewById(R.id.bt2);
+        bt2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(MainActivity.this, ListView2.class);
+
+                startActivity(intent);
+            }
+        });
+    }
+}
